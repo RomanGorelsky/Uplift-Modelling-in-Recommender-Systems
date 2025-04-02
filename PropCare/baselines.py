c8a78946b429df95a63067af46e93de5c22a997e
Tatiana <ttomayly@edu.hse.ru>
2025-04-02 12:21:38 +0300

    Random Base Update

--- PropCare/baselines.py
+++ PropCare/baselines.py
@@ -351,13 +351,13 @@ class LMF(Recommender):
         return pred
 
 class PopularBase(Recommender):
-
     def __init__(self, num_users, num_items,
                  colname_user='idx_user', colname_item='idx_item',
                  colname_outcome='outcome', colname_prediction='pred'):
         super().__init__(num_users=num_users, num_items=num_items,
                          colname_user=colname_user, colname_item=colname_item,
                          colname_outcome=colname_outcome, colname_prediction=colname_prediction)
+        self.df_cnt = df = pd.DataFrame([])
 
     def train(self, df, iter = 1):
         df_cnt = df.groupby(self.colname_item, as_index=False)[self.colname_outcome].sum()
